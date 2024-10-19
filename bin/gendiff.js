2b2e56382072af540dfae608bd9559493dcbeae9
andrew_totmin <andrew.totmin@gmail.com>
2024-10-19 17:01:19 +0500

    add .argumenst and .option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ const program = new Command();
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .description('Compares two configuration files and shows a difference.');
 program.parse();
