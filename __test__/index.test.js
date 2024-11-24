a384e4681ef200a6707b9fec9fbc2c895ec1e239
andrew <andrew.totmin@gmail.com>
2024-11-24 22:17:29 +0500

    add yml files and tes resultYML.txt

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -10,7 +10,12 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const jsonTestFile = readFile('resultJSON.txt');
+const ymlTestFile = readFile('resultYML.txt');
 
 test('testing JSON', () => {
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(jsonTestFile);
+});
+
+test('testing yml', () => {
+  expect(genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml')).toEqual(ymlTestFile);
 });
