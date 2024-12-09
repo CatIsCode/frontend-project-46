2d69bc3ecf1f704167df109f5c84774ef371cd33
andrew <andrew.totmin@gmail.com>
2024-12-09 15:03:23 +0500

    eslint fix

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,7 +1,7 @@
-import genDiff from '../src/index.js';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,13 +9,14 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const jsonTestFile = readFile('resultJSON.txt');
-const ymlTestFile = readFile('resultYML.txt');
-
 test('testing JSON', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(jsonTestFile);
+  const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
+  const actualValue = readFile('resultJSON.txt');
+  expect(expectedValue).toEqual(actualValue);
 });
 
 test('testing yml', () => {
-  expect(genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml')).toEqual(ymlTestFile);
+  const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml');
+  const actualValue = readFile('resultYML.txt');
+  expect(expectedValue).toEqual(actualValue);
 });
