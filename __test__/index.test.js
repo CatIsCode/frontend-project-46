f5df68558ed4281969bb99433b5b39cf0612ede8
andrew <andrew.totmin@gmail.com>
2024-12-15 21:42:23 +0500

    add test for JSON format

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,26 +9,38 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing JSON stylish', () => {
+test('testing json in stylish', () => {
   const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
-  const actualValue = readFile('resultJSON.txt');
+  const actualValue = readFile('resultStylish.txt');
   expect(expectedValue).toEqual(actualValue);
 });
 
-test('testing yml stylish', () => {
+test('testing yml in stylish', () => {
   const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml');
-  const actualValue = readFile('resultYML.txt');
+  const actualValue = readFile('resultStylish.txt');
   expect(expectedValue).toEqual(actualValue);
 });
 
-test('testing JSON plain', () => {
+test('testing json in plain', () => {
   const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
   const actualValue = readFile('resultPlain.txt');
   expect(expectedValue).toEqual(actualValue);
 });
 
-test('testing yml plain', () => {
+test('testing yml in plain', () => {
   const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml', 'plain');
   const actualValue = readFile('resultPlain.txt');
+  expect(expectedValue).toEqual(actualValue);
+});
+
+test('testing json in JSON', () => {
+  const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json');
+  const actualValue = readFile('resultJSON.txt');
+  expect(expectedValue).toEqual(actualValue);
+});
+
+test('testing yml in JSON', () => {
+  const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml', 'json');
+  const actualValue = readFile('resultJSON.txt');
   expect(expectedValue).toEqual(actualValue);
 });
