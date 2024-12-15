f67df3c8a4da6ec2180e328c2e7cf7b0bf41267d
andrew <andrew.totmin@gmail.com>
2024-12-15 18:37:31 +0500

    fix line "nested"

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const stringify = (value, depth) => {
+const stringify = (value, depth = 0) => {
   const iter = (node, depth) => {
     const indent = '  '.repeat(depth * 2);
     if (!_.isObject(node)) {
@@ -20,6 +20,8 @@ const stylish = (tree) => {
     const indent = '  '.repeat(depth * spacesCount - 2);
     const lines = tree.map((node) => {
       switch (node.type) {
+        case 'nested':
+          return `  ${indent}  ${node.key}: ${iter(node.value, depth + 1)}`;
         case 'deleted':
           return `  ${indent}- ${node.key}: ${stringify(node.value, depth)}`;
         case 'added':
@@ -28,10 +30,8 @@ const stylish = (tree) => {
           return `  ${indent}  ${node.key}: ${stringify(node.value, depth)}`;
         case 'changed':
           return `  ${indent}- ${node.key}: ${stringify(node.valueBefore, depth)}\n  ${indent}+ ${node.key}: ${stringify(node.valueAfter, depth)}`;
-        case 'nested':
-          return `  ${indent}  ${node.key}: ${iter(node.value, depth + 1)}`;
         default:
-          throw new Error(`Invalid status - ${node.type}`);
+          throw new Error(`Unknown status - ${node.type}`);
       }
     });
     return ['{', ...lines, `${indent}}`].join('\n');
