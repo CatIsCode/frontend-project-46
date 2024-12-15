d2a4031e38db1f21f70e97fa65b34cedfdd543f3
andrew <andrew.totmin@gmail.com>
2024-12-15 21:21:48 +0500

    add tests for plain format

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,14 +9,26 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('testing JSON', () => {
+test('testing JSON stylish', () => {
   const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json');
   const actualValue = readFile('resultJSON.txt');
   expect(expectedValue).toEqual(actualValue);
 });
 
-test('testing yml', () => {
+test('testing yml stylish', () => {
   const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml');
   const actualValue = readFile('resultYML.txt');
+  expect(expectedValue).toEqual(actualValue);
+});
+
+test('testing JSON plain', () => {
+  const expectedValue = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain');
+  const actualValue = readFile('resultPlain.txt')
+  expect(expectedValue).toEqual(actualValue);
+});
+
+test('testing yml plain', () => {
+  const expectedValue = genDiff('__fixtures__/file3.yml', '__fixtures__/file4.yml', 'plain');
+  const actualValue = readFile('resultPlain.txt')
   expect(expectedValue).toEqual(actualValue);
 });
