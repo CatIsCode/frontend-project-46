510dc3e1d82c45deb760282b8719b9d6f0fbe8f2
andrew <andrew.totmin@gmail.com>
2024-12-15 21:41:51 +0500

    add JSON formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,11 @@
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './JSON.js';
 
 const formatters = {
   plain,
   stylish,
+  json,
 };
 
 const formatter = (ast, format) => formatters[format](ast);
