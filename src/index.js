0363786b58de2cf9290a8d3bd4a18f91a93efbab
andrew <andrew.totmin@gmail.com>
2024-12-15 21:22:20 +0500

    small fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import fs from 'fs';
 import parse from './parse.js';
 import treeBuilder from './treeBuilder.js';
 import stylish from './formatters/stylish.js';
+import formatter from './formatters/index.js';
 
 const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const extractFormat = (filepath) => path.extname(filepath).slice(1);
 const getData = (filepath) => parse(fs.readFileSync(filepath, ('utf-8')), extractFormat(filepath));
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const FullPathFile1 = getFullPath(filepath1);
   const FullPathFile2 = getFullPath(filepath2);
 
@@ -16,7 +17,7 @@ const genDiff = (filepath1, filepath2) => {
   const dataFile2 = getData(FullPathFile2);
 
   const tree = treeBuilder(dataFile1, dataFile2);
-  return stylish(tree);
+  return formatter(tree, format);
 };
 
 export default genDiff;
